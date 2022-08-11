02a7cc3afebea9e739b5d13e274a4b9c21df051c
Samandasyuk Artur <asamandasyuk@ozon.ru>
2022-08-11 13:47:53 +0300

    Template pattern implementation

---
+++ src/Patterns/Template/School.cs
@@ -0,0 +1,13 @@
+namespace Patterns.Template;
+
+public class School : Education
+{
+    public override void Enter() =>
+        Console.WriteLine("Идем в первый класс");
+
+    public override void Study() =>
+        Console.WriteLine("Посещаем уроки, делаем домашние задания");
+
+    public override void GetDocument() =>
+        Console.WriteLine("Получаем аттестат о среднем образовании");
+}
---
+++ tests/Patterns.UnitTests/TemplateTests.cs
@@ -0,0 +1,36 @@
+using Patterns.Template;
+
+namespace Patterns.UnitTests;
+
+public class TemplateTests
+{
+    [Fact]
+    public void School_learn_successful_case()
+    {
+        // Arrange
+        var school = new School();
+        
+        // Act
+        var result = () => school.Learn();
+        
+        // Assert
+        result
+            .Should()
+            .NotThrow();
+    }
+
+    [Fact]
+    public void University_learn_successful_case()
+    {
+        // Arrange
+        var university = new University();
+
+        // Act
+        var result = () => university.Learn();
+
+        // Assert
+        result
+            .Should()
+            .NotThrow();
+    }
+}
